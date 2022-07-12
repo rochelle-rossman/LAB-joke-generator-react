aa7bfe5ff579ed2e635704ca66e5c6186b97e8fc
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-07-12 15:43:40 -0500

    button changes state and shows joke setup on dom

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
-import GetJokeBtn from '../components/getJokeBtn';
+import { useState } from 'react';
+import getJoke from '../api/jokeData';
 
 function Home() {
+  const [joke, setJoke] = useState({});
+  const [btnText, setBtnText] = useState('Get A Joke');
+  const setBtn = (text) => {
+    setBtnText(text);
+  };
+
+  const getAJoke = () => {
+    getJoke().then((obj) => {
+      setJoke({
+        setup: obj.setup,
+        punchline: obj.delivery,
+      });
+
+      setBtn('Get Punchline');
+    });
+  };
+
   return (
-    <div>
-      <GetJokeBtn />
-    </div>
+    <>
+      <button type="button" onClick={getAJoke}>{btnText}</button>
+      <h1>{joke.setup}</h1>
+    </>
   );
 }
 
